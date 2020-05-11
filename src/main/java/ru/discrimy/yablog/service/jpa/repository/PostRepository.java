c7d6daa78ba0ef429b4fb6172f2f69e06005da78
Alexander Bespalov <discrimy.off@gmail.com>
2020-05-11 22:15:34 +0300

    Change @Query way to method name way query

--- src/main/java/ru/discrimy/yablog/service/jpa/repository/PostRepository.java
+++ src/main/java/ru/discrimy/yablog/service/jpa/repository/PostRepository.java
@@ -1,6 +1,5 @@
 package ru.discrimy.yablog.service.jpa.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 import ru.discrimy.yablog.model.Post;
 
@@ -8,6 +7,9 @@ import java.util.List;
 
 @Component
 public interface PostRepository extends BaseRepository<Post> {
-    @Query(" FROM Post WHERE pinned IS TRUE ")
-    List<Post> findAllPinned();
+    List<Post> findByPinned(boolean pinned);
+
+    default List<Post> findAllPinned() {
+        return this.findByPinned(true);
+    }
 }
